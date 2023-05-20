932e8ccd9087b86865decc9d3110587fc9814c7b
Ycaro Gabriell Borges <ycarogborges@gmail.com>
2023-05-20 14:53:01 -0300

    removi bug de scanear letras

--- adivinhe.c
+++ adivinhe.c
@@ -3,6 +3,9 @@
 #include <time.h>
 
 int main(void) {
+
+    system("cls || clear");
+
     int aleatorio, palpite, tentativas = 0;
 
     srand(time(NULL));
@@ -11,19 +14,26 @@ int main(void) {
     printf("Tente adivinhar em qual numero de 1 a 100 eu estou pensando\n\n");
 
     do {
-        printf("digite um numero: ");
+        printf("Digite um numero: ");
 
-        scanf("%d", &palpite);
-        tentativas++;
-        system("clear");
+        int i = scanf("%d", &palpite);
 
-        if(palpite < aleatorio) {
-            printf("\nestou pensando em um numero maior\n\n");
-        } else if(palpite > aleatorio) {
-            printf("\no numero que estou pensando e menor que esse\n\n");
-        } else {
-            printf("\n%d!! isso mesmo, voce acertou em %d tentativas, parabens!\n\n", aleatorio, tentativas);
+        if (i != 1) {
+			fflush(stdin);
+            system("cls || clear");
+            printf("So funciona com numeros.\n\n");
+            continue;
         }
+
+        tentativas++;
+        system("cls || clear");
+
+        if (palpite < aleatorio)
+            printf("Estou pensando em um numero maior.\n\n");
+        else if (palpite > aleatorio)
+            printf("O numero que estou pensando e menor que esse.\n\n");
+        else
+            printf("\t%d!! Isso mesmo, voce acertou em %d tentativas, parabens!\n\n", aleatorio, tentativas);
     }
     while(palpite != aleatorio);
     return 0;
